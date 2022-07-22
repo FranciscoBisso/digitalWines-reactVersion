9930ab30abfaa6b8936dee9a92a7403393a7f9a7
FranciscoBisso <fjbiss@gmail.com>
2022-07-22 18:46:00 -0300

    creo 1ros archivos de rutas y controlador

---
+++ server/src/apiControllers/mainApiController.js
@@ -0,0 +1,7 @@
+const mainApiController = {
+    home: (req, res) => {
+        res.json("Hola");
+    },
+};
+
+module.exports = mainApiController;
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const router = express.Router();
+const mainApiRouter = require("./apiRoutes/mainApiRouter");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", mainApiRouter);
 
 app.listen(3001, () => {
-    console.log("Server working");
+    console.log("Server working in PORT 3001");
 });
