ff5d9373fa09c0db8f5ebccf7473aa42d3837953
FranciscoBisso <fjbisso@gmail.com>
2022-08-02 09:30:11 -0300

    a

--- server/src/routes/mainApiRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mainApiController = require("../controllers/mainApiController");
-
-router.get("/home", mainApiController.home);
-
-module.exports = router;
