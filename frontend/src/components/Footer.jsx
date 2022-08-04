fcffb5c989c3a57672356a7144d0d9f41feca963
FranciscoBisso <fjbisso@gmail.com>
2022-08-04 12:42:55 -0300

    creo componentes de header y footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div class="contenedor">
+                <div class="foot">
+                    <div class="presentacion">
+                        <p>
+                            Hecho con{" "}
+                            <a class="corazon" href="#">
+                                <i class="fas fa-heart"></i>
+                            </a>{" "}
+                            y
+                            <a class="cafe" href="">
+                                <i class="fas fa-coffee"></i>
+                            </a>{" "}
+                            en Mendoza y Buenos Aires, Argentina.
+                        </p>
+                    </div>
+                    <div class="copy">
+                        <p>
+                            &copy; 2021 Digital Wine. Todos los derechos
+                            reservados.
+                        </p>
+                    </div>
+                    <div class="redes">
+                        <ul class="lista">
+                            <a href="#">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <section class="barra-nav">
+                    <a href="/" class="contenedor_logo">
+                        <img
+                            src="/images/logo.png"
+                            alt="Logo"
+                            class="logo"
+                            width="100%"
+                        />
+                        <div class="logo-texto-wrapper">
+                            <p class="logo-texto-D">D</p>
+                            <p class="logo-texto-W">W</p>
+                        </div>
+                    </a>
+                    <nav class="contenedor-links_chica">
+                        <a class="link_chica" href="/products/vinoteca">
+                            <i class="fas fa-wine-bottle"></i>
+                        </a>
+
+                        <a class="link_chica" href="/products/plus">
+                            <i class="fas fa-plus"></i>
+                        </a>
+                        <a class="link_chica" href="/users/cuenta">
+                            <i class="fas fa-user"></i>
+                        </a>
+
+                        <a class="link_chica" href="/users/cava">
+                            <i class="fas fa-wine-glass-alt"></i>
+                        </a>
+                        <a class="link_chica" href="/users/cuenta">
+                            <i class="fas fa-user"></i>
+                        </a>
+
+                        <a class="link_chica" href="/users/login">
+                            <i class="fas fa-user"></i>
+                        </a>
+                        <a class="link_chica" href="/users/registro">
+                            <i class="far fa-id-card"></i>
+                        </a>
+                    </nav>
+
+                    <nav class="contenedor-links_grande">
+                        <a href="/products/vinoteca">Vinoteca</a>
+
+                        <a href="/products/plus">Agregar BVC</a>
+                        <a href="/users/cuenta">Mi Cuenta</a>
+
+                        <a href="/users/cava">Mi Cava</a>
+                        <a href="/users/cuenta">Mi Cuenta</a>
+
+                        <a href="/users/login">Login</a>
+                        <a href="/users/registro">Registro</a>
+                    </nav>
+                </section>
+            </div>
+        );
+    }
+}
