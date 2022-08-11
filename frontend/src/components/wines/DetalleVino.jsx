fb618e64a0497a0d34cdd75a51c9bf746c91fdd2
FranciscoBisso <fjbisso@gmail.com>
2022-08-11 12:34:45 -0300

    meto hooks y fetch a DetalleVino.jsx

--- frontend/src/components/wines/DetalleVino.jsx
+++ frontend/src/components/wines/DetalleVino.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import "../../public/css/wines/DetalleVino.css";
 
 export default function DetalleVino() {
+    let params = useParams();
+    const [wine, setWine] = useState({});
+    useEffect(() => {
+        async function fetchInfo() {
+            let infoRes = await fetch(
+                `http://localhost:3001/api/wines/details/${params.id}`
+            );
+            let newInfo = await infoRes.json();
+
+            setWine(newInfo.data);
+        }
+
+        fetchInfo();
+    }, []);
     return (
         <div>
             <header>
@@ -13,25 +29,25 @@ export default function DetalleVino() {
                 <div className="item-wrapper">
                     <section id="contenedor-detalle">
                         <div className="img-wrapper">
-                            <img src="" alt="vino1" />
+                            <img src={`${wine.imagen}`} alt="vino1" />
                         </div>
                         <div id="contenedor-detalle-vino">
                             <h2></h2>
                             <div className="contenedor-detalle-info">
                                 <h3>Precio:</h3>
-                                <p>$</p>
+                                <p>${wine.precio}</p>
                             </div>
                             <div className="contenedor-detalle-info">
                                 <h3>Bodega:</h3>
-                                <p></p>
+                                <p>{wine.vinoBodega.nombre}</p>
                             </div>
                             <div className="contenedor-detalle-info">
                                 <h3>Variedad:</h3>
-                                <p></p>
+                                <p>{wine.vinoUva.nombre}</p>
                             </div>
                             <div className="contenedor-detalle-info">
                                 <h3>Descripción:</h3>
-                                <p id="descripcion"></p>
+                                <p id="descripcion">{wine.descripcion}</p>
                             </div>
                         </div>
                     </section>
