c24f66d0aa5d38a42111d4e783e3f9a2fcdb4d14
FranciscoBisso <fjbisso@gmail.com>
2022-08-13 19:32:42 -0300

    EditarVino.jsx: problema solucionado. Componente listo (por ahora)

--- frontend/src/components/wines/EditarVino.jsx
+++ frontend/src/components/wines/EditarVino.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
-//import "../../public/css/wines/EditarVino.css";
+import "../../public/css/wines/EditarVino.css";
 
 export default function EditarVino() {
     const params = useParams();
     const [wine, setWine] = useState([]);
     const [cellars, setCellars] = useState([]);
     const [grapes, setGrapes] = useState([]);
+    const [category, setCategory] = useState([]);
 
     useEffect(() => {
         fetchInfo();
@@ -20,188 +21,181 @@ export default function EditarVino() {
             `http://localhost:3001/api/wines/update/${params.id}`
         );
         const wineInfo = await infoRes.json();
-        console.log("Soy wineInfo.data.wine", wineInfo.data.wine);
-        console.log("Soy wineInfo.data.bodegas", wineInfo.data.bodegas);
-        console.log("Soy wineInfo.data.uvas", wineInfo.data.uvas);
+
+        wineInfo.data.wine.bodega = wineInfo.data.wine.vinoBodega.nombre;
+        wineInfo.data.wine.uva = wineInfo.data.wine.vinoUva.nombre;
+        wineInfo.data.wine.categoria = wineInfo.data.wine.vinoCategoria.nombre;
 
         setWine(wineInfo.data.wine);
         setCellars(wineInfo.data.bodegas);
         setGrapes(wineInfo.data.uvas);
+        setGrapes(wineInfo.data.uvas);
+        setCategory(wineInfo.data.categorias);
     };
-
+    console.log(category);
     return (
         <div>
-            <h2>{wine.nombre}</h2>
-            <h2>{wine.precio}</h2>
-            <ul>
-                {cellars.map((cellar, i) => (
-                    <p key={i}>* {cellar.nombre}</p>
-                ))}
-            </ul>
-            <ul>
-                {grapes.map((grape, i) => (
-                    <p key={i}>- {grape.nombre}</p>
-                ))}
-            </ul>
+            <header>
+                <Header />
+            </header>
+            <main className="editarProducto-main-container">
+                <form
+                    className="editarProducto-form-container"
+                    action=""
+                    method="post"
+                    enctype="multipart/form-data"
+                >
+                    <h2>EDITAR PRODUCTO</h2>
+                    <div className="editarProducto-div-img-product">
+                        <label for="imagen" className="form-label">
+                            {" "}
+                            Imagen del producto:
+                        </label>
+                        <input
+                            className="editarProducto-input-img-product form-control box-shadow"
+                            type="file"
+                            accept="image/jpg"
+                            name="imagen"
+                            id="imagen"
+                        />
+                    </div>
+                    <div>
+                        <label for="nombre" className="form-label">
+                            Nombre
+                        </label>
+
+                        <input
+                            className="box-shadow form-control"
+                            type="text"
+                            name="nombre"
+                            id="nombre"
+                            defaultValue={wine.nombre}
+                        />
+                    </div>
+                    <div>
+                        <label for="precio" className="form-label">
+                            Precio:
+                        </label>
+
+                        <input
+                            className="box-shadow form-control"
+                            type="number"
+                            name="precio"
+                            id="precio"
+                            defaultValue={wine.precio}
+                        />
+                    </div>
+                    <div>
+                        <label for="anio" className="form-label">
+                            Anio:
+                        </label>
+
+                        <input
+                            className="box-shadow form-control"
+                            type="number"
+                            name="anio"
+                            id="anio"
+                            defaultValue={wine.anio}
+                        />
+                    </div>
+                    <div>
+                        <label for="stock" className="form-label">
+                            Stock:
+                        </label>
+
+                        <input
+                            className="box-shadow form-control"
+                            type="number"
+                            name="stock"
+                            id="stock"
+                            defaultValue={wine.stock}
+                        />
+                    </div>
+                    <div>
+                        <label for="bodega_id" className="form-label">
+                            Bodega:
+                        </label>
+                        <select
+                            name="bodega_id"
+                            id="bodega_id"
+                            className="form-select box-shadow"
+                        >
+                            <option selected>{wine.bodega}</option>
+                            {cellars.map((cellar, i) => (
+                                <option key={i}>{cellar.nombre}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label for="categoria_id" className="form-label">
+                            Categoría:
+                        </label>
+                        <select
+                            name="categoria_id"
+                            id="categoria_id"
+                            className="form-select box-shadow"
+                        >
+                            <option selected>{wine.categoria}</option>
+                            {category.map((cat, i) => (
+                                <option key={i}>{cat.nombre}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label for="uva_id" className="form-label">
+                            Uva:
+                        </label>
+
+                        <select
+                            name="uva_id"
+                            id="uva_id"
+                            className="form-select box-shadow"
+                        >
+                            <option selected>{wine.uva}</option>
+                            {grapes.map((grape, i) => (
+                                <option key={i}>{grape.nombre}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label for="descripcion" className="form-label">
+                            Descripcion:
+                        </label>
+
+                        <textarea
+                            className="editarProducto-text-area box-shadow form-control"
+                            name="descripcion"
+                            id="descripcion"
+                            cols="30"
+                            rows="10"
+                            defaultValue={wine.descripcion}
+                        ></textarea>
+                    </div>
+
+                    <div className="editarProducto-div-button">
+                        <button
+                            className="editarProducto-button btn-secondary"
+                            type="submit"
+                            defaultValue="Editar producto"
+                        >
+                            EDITAR
+                        </button>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li>{/* meter los errores */}</li>
+                        </ul>
+                    </div>
+
+                    <div className="errores">
+                        <ul className="errores-front"></ul>
+                    </div>
+                </form>
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
-        // <div>
-        //     <header>
-        //         <Header />
-        //     </header>
-        //     <main className="editarProducto-main-container">
-        //         <form
-        //             className="editarProducto-form-container"
-        //             action=""
-        //             method="post"
-        //             enctype="multipart/form-data"
-        //         >
-        //             <h2>EDITAR PRODUCTO</h2>
-        //             <div className="editarProducto-div-img-product">
-        //                 <label for="imagen" className="form-label">
-        //                     {" "}
-        //                     Imagen del producto:{" "}
-        //                 </label>
-        //                 <input
-        //                     className="editarProducto-input-img-product form-control box-shadow"
-        //                     type="file"
-        //                     accept="image/jpg"
-        //                     name="imagen"
-        //                     id="imagen"
-        //                     value=""
-        //                 />
-        //             </div>
-        //             <div>
-        //                 <label for="nombre" className="form-label">
-        //                     Nombre
-        //                 </label>
-
-        //                 <input
-        //                     className="box-shadow form-control"
-        //                     type="text"
-        //                     name="nombre"
-        //                     id="nombre"
-        //                     value=""
-        //                 />
-        //             </div>
-        //             <div>
-        //                 <label for="precio" className="form-label">
-        //                     Precio:
-        //                 </label>
-
-        //                 <input
-        //                     className="box-shadow form-control"
-        //                     type="number"
-        //                     name="precio"
-        //                     id="precio"
-        //                     value=""
-        //                 />
-        //             </div>
-        //             <div>
-        //                 <label for="anio" className="form-label">
-        //                     Anio:
-        //                 </label>
-
-        //                 <input
-        //                     className="box-shadow form-control"
-        //                     type="number"
-        //                     name="anio"
-        //                     id="anio"
-        //                     placeholder=" Ej: 2017"
-        //                     value=""
-        //                 />
-        //             </div>
-        //             <div>
-        //                 <label for="stock" className="form-label">
-        //                     Stock:
-        //                 </label>
-
-        //                 <input
-        //                     className="box-shadow form-control"
-        //                     type="number"
-        //                     name="stock"
-        //                     id="stock"
-        //                     placeholder=" Ej: 08"
-        //                     value=""
-        //                 />
-        //             </div>
-        //             <div>
-        //                 <label for="bodega_id" className="form-label">
-        //                     Bodega:
-        //                 </label>
-        //                 <select
-        //                     name="bodega_id"
-        //                     id="bodega_id"
-        //                     className="form-select box-shadow"
-        //                 >
-        //                     <option value="">{/* meter las bodagas */}</option>
-        //                 </select>
-        //             </div>
-        //             <div>
-        //                 <label for="categoria_id" className="form-label">
-        //                     Categoría:
-        //                 </label>
-        //                 <select
-        //                     name="categoria_id"
-        //                     id="categoria_id"
-        //                     className="form-select box-shadow"
-        //                 >
-        //                     <option value="">
-        //                         {/* meter las categorías */}
-        //                     </option>
-        //                 </select>
-        //             </div>
-        //             <div>
-        //                 <label for="uva_id" className="form-label">
-        //                     Uva:
-        //                 </label>
-        //                 <select
-        //                     name="uva_id"
-        //                     id="uva_id"
-        //                     className="form-select box-shadow"
-        //                 >
-        //                     <option value="">
-        //                         {/* meter el tipo de uva */}
-        //                     </option>
-        //                 </select>
-        //             </div>
-        //             <div>
-        //                 <label for="descripcion" className="form-label">
-        //                     Descripcion:
-        //                 </label>
-
-        //                 <textarea
-        //                     className="editarProducto-text-area box-shadow form-control"
-        //                     name="descripcion"
-        //                     id="descripcion"
-        //                     cols="30"
-        //                     rows="10"
-        //                 ></textarea>
-        //             </div>
-
-        //             <div className="editarProducto-div-button">
-        //                 <button
-        //                     className="editarProducto-button btn-secondary"
-        //                     type="submit"
-        //                     value="Editar producto"
-        //                 >
-        //                     EDITAR
-        //                 </button>
-        //             </div>
-
-        //             <div>
-        //                 <ul>
-        //                     <li>{/* meter los errores */}</li>
-        //                 </ul>
-        //             </div>
-
-        //             <div className="errores">
-        //                 <ul className="errores-front"></ul>
-        //             </div>
-        //         </form>
-        //     </main>
-        //     <footer>
-        //         <Footer />
-        //     </footer>
-        // </div>
     );
 }
