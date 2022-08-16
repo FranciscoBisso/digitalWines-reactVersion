438a44a77d902fdf82e0927e836b2743afbc9160
FranciscoBisso <fjbisso@gmail.com>
2022-08-16 16:33:10 -0300

    corrijo en backend/app.js linea 12 express.static()

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(methodOverride("_method"));
 app.use(cors({ origin: "*" }));
 
