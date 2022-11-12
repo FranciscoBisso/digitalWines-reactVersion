4953055229c30ae761775abeb02b0e2c62217f26
FranciscoBisso <fjbisso@gmail.com>
2022-11-12 19:21:39 -0300

    funcionalidad de loggeo

--- backend/src/controllers/userApiController.js
+++ backend/src/controllers/userApiController.js
@@ -3,7 +3,11 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const createToken = (id) => {
-	return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3d" });
+	return jwt.sign({ id }, process.env.SECRET);
+	// función para crear y firmar los tokens para re-usarla c/vez que sea necesario.
+	// el 1er argumento es el payload, que no tiene que tener info sensible, y se corresponde con el argumento que recibe la función.
+	// el 2do argumento es la clave secreta que solo tiene que ser conocida por el servidor.
+	// puede tener un 3er argumento donde establecemos opciones como durabilidad y demás cosas.
 };
 
 const userApiController = {
@@ -14,10 +18,10 @@ const userApiController = {
 
 		if (!existingEmail) {
 			const newUser = await db.Usuarios.create({
+				//chequear que los inputs tengan estos nombres.
 				nombre: req.body.nombre,
 				email: req.body.email,
-				contrasenia: req.body.contrasenia,
-				//contrasenia: bcryptjs.hashSync(req.body.password, 10),
+				contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
 				tipo_id: 2,
 				imagen: req.body.imagen,
 				//imagen: req.file.path.split("public").pop(),
@@ -26,16 +30,49 @@ const userApiController = {
 			const token = createToken(newUser.id);
 
 			res.status(200).json({
-				email: newUser.email,
+				newUser: {
+					nombre: newUser.nombre,
+					email: newUser.email,
+					imagen: newUser.imagen,
+				},
 				token: token,
 			});
 		}
 		if (existingEmail) {
-			res.status(400).json({ credentialsError: "Invalid credentials" });
+			res.status(400).json({
+				credentialsError: "Credenciales inválidas.",
+			});
 		}
 	},
 
-	login: async (req, res) => {},
+	login: async (req, res) => {
+		const user = await db.Usuarios.findOne({
+			where: { email: req.body.email },
+		});
+
+		if (user) {
+			const match = bcryptjs.compareSync(
+				req.body.contrasenia,
+				user.contrasenia
+			);
+			if (match) {
+				const token = createToken(user.id);
+
+				res.status(200).json({
+					loggedUser: {
+						nombre: user.nombre,
+						email: user.email,
+						imagen: user.imagen,
+					},
+					token: token,
+				});
+			}
+		} else {
+			res.status(400).json({
+				credentialsError: "Credenciales inválidas.",
+			});
+		}
+	},
 
 	details: async (req, res) => {
 		const user = await db.Usuarios.findOne({
