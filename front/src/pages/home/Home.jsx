ed81c2fb6c78be657a96025f153cf62b41935943
FranciscoBisso <fjbisso@gmail.com>
2023-09-06 22:07:48 -0300

    small fixes to <Homw> styles

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -9,47 +9,57 @@ import styles from "./home.module.css";
 const url = "http://localhost:3001/api/home";
 
 function Home({ pageTitle }) {
-  const { data, status } = useQuery({
-    queryKey: ["homeInfo"],
-    queryFn: () => fetchData(url),
-  });
+    const { data, status } = useQuery({
+        queryKey: ["homeInfo"],
+        queryFn: () => fetchData(url),
+    });
 
-  console.log("data -->", data);
+    console.log("data -->", data);
 
-  return (
-    <>
-      <Helmet>
-        <title>{pageTitle}</title>
-        <meta
-          name="description"
-          content="¡Bienvenido a nuestra página principal!"
-        />
-      </Helmet>
-
-      {status === "error" && <NotFound />}
-      {status === "loading" && <div className={styles.loading}>Loading...</div>}
-      {status === "success" && (
+    return (
         <>
-          <section className={styles.intro_section}>
-            <div className={styles.video_wrapper}>
-              <video className={styles.video} playsInline autoPlay loop muted>
-                <source src={video} type="video/mp4" />
-              </video>
-            </div>
-            <div className={styles.slider_wrapper}>
-              <WineSlider wines={data.data.destacados} />
-            </div>
-          </section>
-          <section className={styles.wine_card_wrapper}>
-            <WineSlider wines={data.data.masEconomicos} />
-          </section>
-          <section className={styles.wine_card_wrapper}>
-            <WineSlider wines={data.data.masVendidos} />
-          </section>
+            <Helmet>
+                <title>{pageTitle}</title>
+                <meta
+                    name="description"
+                    content="¡Bienvenido a nuestra página principal!"
+                />
+            </Helmet>
+
+            {status === "error" && <NotFound />}
+            {status === "loading" && (
+                <div className={styles.loading}>Loading...</div>
+            )}
+            {status === "success" && (
+                <>
+                    <section className={styles.intro_section}>
+                        <div className={styles.video_wrapper}>
+                            <video
+                                className={styles.video}
+                                playsInline
+                                autoPlay
+                                loop
+                                muted
+                            >
+                                <source src={video} type="video/mp4" />
+                            </video>
+                        </div>
+                        <div className={styles.slider_wrapper}>
+                            <WineSlider wines={data.data.destacados} />
+                        </div>
+                    </section>
+
+                    <section>
+                        <WineSlider wines={data.data.masEconomicos} />
+                    </section>
+
+                    <section>
+                        <WineSlider wines={data.data.masVendidos} />
+                    </section>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 export default Home;
