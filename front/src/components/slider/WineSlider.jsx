b6e4a9b73e9256a2bf4132153dd84a61463fa0f0
FranciscoBisso <fjbiss@gmail.com>
2023-09-07 12:04:23 -0300

    Home styles

--- front/src/components/slider/WineSlider.jsx
+++ front/src/components/slider/WineSlider.jsx
@@ -1,16 +1,19 @@
 import styles from "./wineSlider.module.css";
 
 export default function WinesSlider({ wines, title }) {
-    return (
-        <>
-            <span className={styles.title}>{title}</span>
-            <div className={styles.slider}>
-                {wines.map((wine) => (
-                    <div key={wine.id} className={styles.slider_item}>
-                        <img src={wine.imagen} />
-                    </div>
-                ))}
+  return (
+    <>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.slider}>
+        {wines.map((wine) => (
+          <>
+            <div key={wine.id} className={styles.slider_item}>
+              <img src={wine.imagen} />
             </div>
-        </>
-    );
+            <div className={styles.wine_description}></div>
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
