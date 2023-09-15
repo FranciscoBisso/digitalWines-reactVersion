95e1513d2bcd70735ff6c5ba10b09a647fe2b920
FranciscoBisso <fjbiss@gmail.com>
2023-09-15 11:21:27 -0300

    modal bug fixed

--- front/src/components/slider/WineSlider.jsx
+++ front/src/components/slider/WineSlider.jsx
@@ -1,51 +1,58 @@
 import PropTypes from "prop-types";
 import styles from "./wineSlider.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function WinesSlider({ wines, title }) {
-    const modal = useRef();
+  const modal = useRef();
+  const [selectedWine, setSelectedWine] = useState(null);
 
-    const open = (e) => {
-        e.stopPropagation();
-        modal?.current.showModal();
-    };
-    const close = (e) => {
-        e.stopPropagation();
-        modal?.current.close();
-    };
+  const open = (wine) => {
+    setSelectedWine(wine);
+    modal?.current.showModal();
+  };
+  const close = (e) => {
+    e.stopPropagation();
+    modal?.current.close();
+    setSelectedWine(null);
+  };
 
-    return (
-        <>
-            <span className={styles.title}>{title}</span>
-            <div className={styles.slider}>
-                {wines.map((wine) => (
-                    <div
-                        key={wine.id}
-                        className={styles.slider_item}
-                        onClick={open}
-                    >
-                        <img src={wine.imagen} loading="lazy" />
-                        <dialog className={styles.modal} ref={modal}>
-                            <img src={wine.imagen} loading="lazy" />
-                            <div className={styles.wine_description}>
-                                <h4 className={styles.name}>{wine.nombre}</h4>
-                                <h5 className={styles.vineyard}>
-                                    {wine.vinoBodega.nombre}
-                                </h5>
-                                <h6
-                                    className={styles.price}
-                                >{`AR$ ${wine.precio}`}</h6>
-                                <button onClick={close}>X</button>
-                            </div>
-                        </dialog>
-                    </div>
-                ))}
+  return (
+    <>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.slider}>
+        {wines.map((wine) => (
+          <div
+            key={wine.id}
+            className={styles.slider_item}
+            onClick={() => {
+              open(wine);
+            }}
+          >
+            <img src={wine.imagen} loading="lazy" />
+          </div>
+        ))}
+      </div>
+
+      <dialog className={styles.modal} ref={modal}>
+        {selectedWine && (
+          <>
+            <img src={selectedWine.imagen} loading="lazy" />
+            <div className={styles.wine_description}>
+              <h4 className={styles.name}>{selectedWine.nombre}</h4>
+              <h5 className={styles.vineyard}>
+                {selectedWine.vinoBodega.nombre}
+              </h5>
+              <h6 className={styles.price}>{`AR$ ${selectedWine.precio}`}</h6>
+              <button onClick={close}>X</button>
             </div>
-        </>
-    );
+          </>
+        )}
+      </dialog>
+    </>
+  );
 }
 
 WinesSlider.propTypes = {
-    title: PropTypes.string,
-    wines: PropTypes.array,
+  title: PropTypes.string,
+  wines: PropTypes.array,
 };
