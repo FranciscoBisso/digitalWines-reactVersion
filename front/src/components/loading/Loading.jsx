a46624b3759e152de7f65bd26bcf5d1a0a4f4aa8
FranciscoBisso <fjbisso@gmail.com>
2023-09-23 20:44:22 -0300

    modal css

--- front/src/components/loading/Loading.jsx
+++ front/src/components/loading/Loading.jsx
@@ -3,13 +3,13 @@ import { faWineBottle } from "@fortawesome/free-solid-svg-icons";
 import styles from "./loading.module.css";
 
 export default function Loading() {
-    return (
-        <div className={styles.loading_wrapper}>
-            <FontAwesomeIcon
-                className={styles.loading_spinner}
-                icon={faWineBottle}
-                spin
-            />
-        </div>
-    );
+	return (
+		<div className={styles.loading_wrapper}>
+			<FontAwesomeIcon
+				className={styles.loading_spinner}
+				icon={faWineBottle}
+				beatFade
+			/>
+		</div>
+	);
 }
