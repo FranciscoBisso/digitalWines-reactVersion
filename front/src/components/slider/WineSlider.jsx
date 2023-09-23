c2e7f0b73361d3cdefd36cd74313aeca12c27d0a
FranciscoBisso <fjbisso@gmail.com>
2023-09-23 14:55:15 -0300

    home: css

--- front/src/components/slider/WineSlider.jsx
+++ front/src/components/slider/WineSlider.jsx
@@ -1,51 +1,54 @@
-import PropTypes from "prop-types"
-import styles from "./wineSlider.module.css"
-import {useRef, useState} from "react"
-import {Link} from "react-router-dom"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import PropTypes from "prop-types";
+import styles from "./wineSlider.module.css";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faXmark,
 	faWineGlass,
 	faStar,
 	faAngleDown,
-} from "@fortawesome/free-solid-svg-icons"
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function WinesSlider({wines, title}) {
-	const modal = useRef()
-	const [selectedWine, setSelectedWine] = useState(null)
+export default function WinesSlider({ wines, title }) {
+	const modal = useRef();
+	const [selectedWine, setSelectedWine] = useState(null);
 
 	const open = (wine) => {
-		setSelectedWine(wine)
-		modal?.current.showModal()
-	}
+		setSelectedWine(wine);
+		modal?.current.showModal();
+	};
 	const close = (e) => {
-		e.stopPropagation()
-		modal?.current.close()
-		setSelectedWine(null)
-	}
+		e.stopPropagation();
+		modal?.current.close();
+		setSelectedWine(null);
+	};
 
 	return (
 		<>
-			<span className={styles.title}>{title}</span>
-			<div className={styles.slider}>
-				{wines.map((wine) => (
-					<div
-						key={wine.id}
-						className={styles.slider_item}
-						onClick={() => {
-							open(wine)
-						}}
-					>
-						<img
-							className={styles.item_img}
-							src={wine.imagen}
-							loading="lazy"
-						/>
-					</div>
-				))}
+			<div>
+				<span className={styles.title}>{title}</span>
+				<div className={styles.slider}>
+					{wines.map((wine) => (
+						<article
+							key={wine.id}
+							className={styles.slider_item}
+							onClick={() => {
+								open(wine);
+							}}>
+							<img
+								className={styles.item_img}
+								src={wine.imagen}
+								loading="lazy"
+							/>
+						</article>
+					))}
+				</div>
 			</div>
 
-			<dialog className={styles.modal} ref={modal}>
+			<dialog
+				className={styles.modal}
+				ref={modal}>
 				{selectedWine && (
 					<div className={styles.card}>
 						<img
@@ -59,8 +62,9 @@ export default function WinesSlider({wines, title}) {
 								{selectedWine.nombre}
 							</h4>
 							<h5
-								className={styles.price}
-							>{`AR$ ${selectedWine.precio}`}</h5>
+								className={
+									styles.price
+								}>{`AR$ ${selectedWine.precio}`}</h5>
 						</div>
 						<div className={styles.buttons_wrapper}>
 							<button className={styles.actions}>
@@ -73,25 +77,23 @@ export default function WinesSlider({wines, title}) {
 						<button className={styles.actions}>
 							<Link
 								className={`${styles.actions} ${styles.link_button}`}
-								to={`detalle/${selectedWine.id}`}
-							>
+								to={`detalle/${selectedWine.id}`}>
 								<FontAwesomeIcon icon={faAngleDown} />
 							</Link>
 						</button>
 						<button
 							className={`${styles.actions} ${styles.exit_button}`}
-							onClick={close}
-						>
+							onClick={close}>
 							<FontAwesomeIcon icon={faXmark} />
 						</button>
 					</div>
 				)}
 			</dialog>
 		</>
-	)
+	);
 }
 
 WinesSlider.propTypes = {
 	title: PropTypes.string,
 	wines: PropTypes.array,
-}
+};
