636b9a742c19222dbf28af492f2c33952c10b5e6
FranciscoBisso <fjbiss@gmail.com>
2023-12-27 17:24:08 -0300

    Details: css

--- front/src/pages/wines/details/Details.jsx
+++ front/src/pages/wines/details/Details.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import { lazy } from "react";
 import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
@@ -12,7 +11,7 @@ const Loading = lazy(() => import("../../../components/loading/Loading"));
 const NotFound = lazy(() => import("../../notFound/NotFound"));
 const WineSlider = lazy(() => import("../../../components/slider/WineSlider"));
 
-export default function Details({ pageTitle }) {
+export default function Details() {
 	const { id } = useParams();
 
 	const detailsQuery = useQuery({
@@ -23,32 +22,56 @@ export default function Details({ pageTitle }) {
 
 	return (
 		<>
-			<Helmet>
-				<title>{pageTitle}</title>
-				<link
-					rel="icon"
-					href="../../../assets/icons/logo.ico"
-				/>
-
-				<meta
-					name="description"
-					content="¡Bienvenido a nuestra página para ver los detalles del vino!"
-				/>
-			</Helmet>
-
 			{detailsQuery.isError && (
 				<NotFound apiErrorMsg={detailsQuery.error?.message} />
 			)}
 			{detailsQuery.isLoading && <Loading />}
 			{detailsQuery.isSuccess && detailsQuery.data && (
 				<>
-					<article className={styles.wine_card}>
-						<img
-							className={styles.wine_card_img}
-							src={detailsQuery.data.wine.imagen}
-							alt="wineImg"
-							loading="lazy"
+					<Helmet>
+						<title>
+							{detailsQuery.data.wine.nombre.toUpperCase()}
+						</title>
+						<link
+							rel="icon"
+							href="../../../assets/icons/logo.ico"
 						/>
+
+						<meta
+							name="description"
+							content="¡Bienvenido a nuestra página para ver los detalles del vino!"
+						/>
+					</Helmet>
+					<article className={styles.wine_card}>
+						<div className={styles.wine_card_img_wrapper}>
+							<img
+								className={styles.wine_card_img}
+								src={detailsQuery.data.wine.imagen}
+								alt="wineImg"
+								loading="lazy"
+							/>
+							<div className={styles.numbers_wrapper}>
+								<h4 className={styles.wine_price}>
+									<strong>
+										${detailsQuery.data.wine.precio}
+									</strong>
+								</h4>
+							</div>
+							<div className={styles.actions_wrapper}>
+								<button className={styles.actions}>
+									<FontAwesomeIcon icon={faStar} />
+									<span className={styles.actions_subtitle}>
+										Favoritos
+									</span>
+								</button>
+								<button className={styles.actions}>
+									<FontAwesomeIcon icon={faWineGlass} />
+									<span className={styles.actions_subtitle}>
+										Cava
+									</span>
+								</button>
+							</div>
+						</div>
 
 						<div className={styles.wine_card_body}>
 							<h1 className={styles.wine_name}>
@@ -60,30 +83,9 @@ export default function Details({ pageTitle }) {
 							<h3 className={styles.wine_grape}>
 								{detailsQuery.data.wine.vinoUva.nombre}
 							</h3>
-							<div className={styles.numbers_wrapper}>
-								<h4 className={styles.wine_price}>
-									<strong>
-										${detailsQuery.data.wine.precio}
-									</strong>
-								</h4>
-							</div>
 							<p className={styles.wine_description}>
 								{detailsQuery.data.wine.descripcion}
 							</p>
-						</div>
-						<div className={styles.actions_wrapper}>
-							<button className={styles.actions}>
-								<FontAwesomeIcon icon={faStar} />
-								<span className={styles.actions_subtitle}>
-									Favoritos
-								</span>
-							</button>
-							<button className={styles.actions}>
-								<FontAwesomeIcon icon={faWineGlass} />
-								<span className={styles.actions_subtitle}>
-									Cava
-								</span>
-							</button>
 						</div>
 					</article>
 					{detailsQuery.data.similarWines.length != 0 && (
@@ -100,7 +102,3 @@ export default function Details({ pageTitle }) {
 		</>
 	);
 }
-
-Details.propTypes = {
-	pageTitle: PropTypes.string,
-};
