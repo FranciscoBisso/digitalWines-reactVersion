2c4e28436d4f4e0f8a7fe7e6aaa8a9e2aafa20fb
FranciscoBisso <fjbiss@gmail.com>
2023-12-30 22:03:08 -0300

    Login: css

--- front/src/pages/users/login/Login.jsx
+++ front/src/pages/users/login/Login.jsx
@@ -1,20 +1,49 @@
 import PropTypes from "prop-types";
+import styles from "./login.module.css";
 import { Helmet } from "react-helmet";
 
 export default function Login({ pageTitle }) {
-    return (
-        <>
-            <Helmet>
-                <title>{pageTitle}</title>
-                <meta
-                    name="description"
-                    content="¡Bienvenido a nuestra página de login de usuarios!"
-                />
-            </Helmet>
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>{pageTitle}</title>
+				<meta
+					name="description"
+					content="¡Bienvenido a nuestra página de login de usuarios!"
+				/>
+			</Helmet>
+			<div className={styles.form_wrapper}>
+				<form className={styles.form}>
+					<h1 className={styles.form_title}>LOGIN</h1>
+
+					<button
+						type="button"
+						className={styles.google_btn}>
+						google
+					</button>
+
+					<input
+						type="email"
+						className={styles.inputs}
+						placeholder="Email"
+					/>
+					<input
+						type="password"
+						className={styles.inputs}
+						placeholder="Password"
+					/>
+
+					<button
+						type="button"
+						className={styles.enter_btn}>
+						Enviar
+					</button>
+				</form>
+			</div>
+		</>
+	);
 }
 
 Login.propTypes = {
-    pageTitle: PropTypes.string,
+	pageTitle: PropTypes.string,
 };
--- front/src/pages/users/register/Register.jsx
+++ front/src/pages/users/register/Register.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
+import styles from "./register.module.css";
 
 export default function Register({ pageTitle }) {
-    return (
-        <>
-            <Helmet>
-                <title>{pageTitle}</title>
-                <meta
-                    name="description"
-                    content="¡Bienvenido a nuestra página de registro de usuarios!"
-                />
-            </Helmet>
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>{pageTitle}</title>
+				<meta
+					name="description"
+					content="¡Bienvenido a nuestra página de registro de usuarios!"
+				/>
+			</Helmet>
+			<form className={styles.register_form}></form>
+		</>
+	);
 }
 
 Register.propTypes = {
-    pageTitle: PropTypes.string,
+	pageTitle: PropTypes.string,
 };
