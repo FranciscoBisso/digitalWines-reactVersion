2cd60cde6b1520449945712610ad721af90052d9
FranciscoBisso <fjbiss@gmail.com>
2024-01-09 18:42:42 -0300

    Install NextUI

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../index.css";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-            {/* <ReactQueryDevtools initialIsOpen={false} />; */}
-        </QueryClientProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<NextUIProvider>
+				<App />
+				{/* <ReactQueryDevtools initialIsOpen={false} />; */}
+			</NextUIProvider>
+		</QueryClientProvider>
+	</React.StrictMode>
 );
