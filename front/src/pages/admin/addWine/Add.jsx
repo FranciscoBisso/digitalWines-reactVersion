7a99e68cfafb325590baf74a2317afd5bb8a6559
FranciscoBisso <fjbisso@gmail.com>
2024-01-10 18:00:10 -0300

    Add.jsx: working on new form with MUI

--- front/src/pages/admin/addWine/Add.jsx
+++ front/src/pages/admin/addWine/Add.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
-import { lazy } from "react";
+import { lazy, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { addWineUrl } from "../../../services/urls";
 import { get } from "../../../services/fetchData";
-import styles from "./add.module.css";
+// import styles from "./add.module.css";
+import styles from "./newWine.module.css";
+
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
 import Button from "@mui/material/Button";
+
 const Loading = lazy(() => import("../../../components/loading/Loading"));
 const NotFound = lazy(() => import("../../../components/notFound/NotFound"));
 
@@ -14,6 +23,11 @@ export default function Add({ pageTitle }) {
 		queryKey: ["addQuery"],
 		queryFn: () => get(addWineUrl),
 	});
+
+	const [selectedItem, setSelectedItem] = useState("");
+	const handleChange = (event) => {
+		setSelectedItem(event.target.value);
+	};
 
 	return (
 		<>
@@ -31,19 +45,119 @@ export default function Add({ pageTitle }) {
 			{addQuery.isSuccess && (
 				<div className={styles.form_wrapper}>
 					<form className={styles.form}>
-						<h1 className={styles.form_title}>AGREGAR</h1>
-						<Button variant="standard">Hello world</Button>
-						<label
-							htmlFor="imagen"
-							className={styles.label}>
-							Imagen del vino:
-							<input
-								type="file"
-								name="imagen"
-								id="imagen"
-								className={styles.upload_img_input}
-							/>
-						</label>
+						<h1 className={styles.form_title}>AGREGAR VINO</h1>
+						<TextField
+							type="file"
+							name="imagen"
+							id="imagen"
+							label="Imagen del vino:"
+							variant="standard"
+							// error={}
+							helperText={""}
+							className={styles.text_field}
+						/>
+						<TextField
+							id="nombre"
+							label="Nombre:"
+							placeholder="Ej: Toro Viejo"
+							variant="standard"
+							className={styles.text_field}
+						/>
+						<FormControl
+							variant="standard"
+							className={styles.select_wrapper}>
+							<InputLabel
+								id="label_bodega_id"
+								className={styles.label}>
+								Bodega:
+							</InputLabel>
+							<Select
+								labelId="label_bodega_id"
+								name="bodega_id"
+								id="bodega_id"
+								label="bodega_id"
+								className={styles.select}
+								value={selectedItem}
+								onChange={handleChange}>
+								{addQuery.data.bodegas?.map((winery, index) => (
+									<MenuItem
+										className={styles.select_items}
+										key={index}
+										value={winery.id}>
+										{winery.nombre}
+									</MenuItem>
+								))}
+							</Select>
+							<FormHelperText>{}</FormHelperText>
+						</FormControl>
+
+						<FormControl
+							variant="standard"
+							className={styles.select_wrapper}>
+							<InputLabel
+								id="label_uva_id"
+								className={styles.label}>
+								Varietal:
+							</InputLabel>
+							<Select
+								labelId="label_uva_id"
+								name="uva_id"
+								id="uva_id"
+								label="uva_id"
+								className={styles.select}
+								value={selectedItem}
+								onChange={handleChange}>
+								{addQuery.data.varietales?.map(
+									(varietal, index) => (
+										<MenuItem
+											className={styles.select_items}
+											key={index}
+											value={varietal.id}>
+											{varietal.nombre}
+										</MenuItem>
+									)
+								)}
+							</Select>
+							<FormHelperText>{}</FormHelperText>
+						</FormControl>
+
+						<FormControl
+							variant="standard"
+							className={styles.select_wrapper}>
+							<InputLabel
+								id="label_categoria_id"
+								className={styles.label}>
+								Categoría:
+							</InputLabel>
+							<Select
+								labelId="label_categoria_id"
+								name="categoria_id"
+								id="categoria_id"
+								label="categoria_id"
+								className={styles.select}
+								value={selectedItem}
+								onChange={handleChange}>
+								{addQuery.data.categorias?.map(
+									(category, index) => (
+										<MenuItem
+											className={styles.select_items}
+											key={index}
+											value={category.id}>
+											{category.nombre}
+										</MenuItem>
+									)
+								)}
+							</Select>
+							<FormHelperText>{}</FormHelperText>
+						</FormControl>
+
+						<Button
+							variant="text"
+							className={styles.button}>
+							Agregar
+						</Button>
+					</form>
+					{/* <form className={styles.form}>
 						<label
 							htmlFor="nombre"
 							className={styles.label}>
@@ -146,34 +260,7 @@ export default function Add({ pageTitle }) {
 								))}
 							</select>
 						</label>
-						<label
-							htmlFor="uva_id"
-							className={styles.label}>
-							Varietal:
-							<select
-								name="uva_id"
-								id="uva_id"
-								className={styles.select}
-								placeholder="Varietal">
-								<option
-									value=""
-									disabled
-									selected
-									hidden
-									className={styles.default_option}>
-									Selecciona una opción
-								</option>
-								{addQuery.data.varietales?.map(
-									(varietal, index) => (
-										<option
-											key={index}
-											value={varietal.id}>
-											{varietal.nombre}
-										</option>
-									)
-								)}
-							</select>
-						</label>
+						
 						<label
 							htmlFor="descripcion"
 							className={styles.label}>
@@ -190,7 +277,7 @@ export default function Add({ pageTitle }) {
 							className={styles.enter_btn}>
 							Agregar
 						</button>
-					</form>
+					</form> */}
 				</div>
 			)}
 		</>
